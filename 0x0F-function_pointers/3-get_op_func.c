d15f461672df2f3a11e72c79ba2322c1f3badb52
Nancy Idiong <80394179+Nancyiddy@users.noreply.github.com>
2022-10-12 08:38:59 +0100

    3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -4,7 +4,6 @@
  * get_op_func - selects the correct function to perform
  * the operation asked by the user.
  * @s: char operator.
- *
  * Return: pointer to the function that corresponds to the operator.
  */
 int (*get_op_func(char *s))(int, int)

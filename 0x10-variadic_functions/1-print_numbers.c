8522593d27e78b13e75fc66d9a6c66bdb3100abb
Nancy Idiong <80394179+Nancyiddy@users.noreply.github.com>
2022-10-13 17:42:42 +0100

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -4,7 +4,6 @@
  * print_numbers - prints numbers.
  * @separator: string to be printed between numbers.
  * @n: number of integers passed to the function.
- *
  * Return: no return.
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
